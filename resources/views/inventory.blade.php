396767ad5e42b9fbb186156797d44c57258512df
Jenmar Alano <jenmaracantilado.alano@gmail.com>
2025-08-22 02:16:00 -0700

    Deleted

--- resources/views/inventory.blade.php
+++ resources/views/inventory.blade.php
@@ -73,7 +73,7 @@
                     </div>
                 </div>
             </div>
-            <h1>dassad</h1>
+            
             <!-- Modal Component: Add Batch -->
             <x-show-modal :showModal="'showModal'" :action="route('inventory.store')" title="Add Item" submitText="Create">
                 @csrf
